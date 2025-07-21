a884c53596bc4931af98226c81bb87e62a65b7da
puttdlc <pacharapolpadungkarn@gmail.com>
2025-07-21 16:57:34 +0700

    Delete c1q3.py

--- c1q3.py
+++
@@ -1,8 +0,0 @@
-print(" *** Summation of each digit ***")
-inp = input("Enter a positive number : ")
-
-summation = 0
-for i in range(0,len(inp)):
-    summation += int(inp[i])
-
-print(f"Summation of each digit =  {summation}")
